17ec9fde1789d49fb5152547267e22c4fd8f64d4
Naveen Bojja <bojjanaveenraj0081@gmail.com>
2024-09-25 08:53:04 +0530

    Update Todo.java

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,3 +1,4 @@
+
 package com.example.todo.model;
 
 import javax.persistence.*;
@@ -8,16 +9,18 @@ public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
     private int id;
 
     @Column(name = "todo")
     private String todo;
 
-    @Column(name = "status")
-    private String status;
-
     @Column(name = "priority")
     private String priority;
+
+    @Column(name = "status")
+    private String status;
 
     public Todo() {
     }
@@ -25,8 +28,9 @@ public class Todo {
     public Todo(int id, String todo, String priority, String status) {
         this.id = id;
         this.todo = todo;
-        this.status = status;
         this.priority = priority;
+        this.status = status;
+
     }
 
     public int getId() {
